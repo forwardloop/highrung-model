f098ee98dda75f9a0ad926cce784cc26d93ec71a
kris <kris.lachor@gmail.com>
2018-02-20 19:28:01 +0000

    Maven central. Tests.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 [![License: GPL v3](https://img.shields.io/badge/license-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/com.github.forwardloop/highrung-model_2.11/badge.svg)](https://maven-badges.herokuapp.com/maven-central/com.github.forwardloop/highrung-model_2.11)
 [![Build Status](https://travis-ci.org/forwardloop/highrung-model.svg)](https://travis-ci.org/forwardloop/highrung-model)
--- src/test/scala/highrung/model/MatchResultSpec.scala
+++ src/test/scala/highrung/model/MatchResultSpec.scala
@@ -4,14 +4,14 @@ import org.specs2.mutable._
 
 class MatchResultSpec extends Specification {
 
-  "parse match result" should {
+  "parse match result from string" should {
 
-    "produce 1 game from 1:0 (1:0)" in {
+    "produce detailed with 1 game from 1:0 (1:0)" in {
       val games = List(Game(1, 0))
       MatchResult("1:0 (1:0)") must beEqualTo(Detailed(games))
     }
 
-    "produce 2 games from 2:0 (1:0,9:0) " in {
+    "produce detailed with 2 games from 2:0 (1:0,9:0) " in {
       val games = List(Game(1, 0), Game(9, 0))
       MatchResult("2:0 (1:0,9:0)") must beEqualTo(Detailed(games))
     }
@@ -21,17 +21,16 @@ class MatchResultSpec extends Specification {
       MatchResult("1:2 (1:0,0:9,4:5)") must beEqualTo(Detailed(games))
     }
 
-    "produce Undefined for a 'forfeit'" in {
+    "produce undefined for a 'forfeit'" in {
       MatchResult("forfeit") must beEqualTo(Undefined)
     }
 
-    "produce Undefined for result with incorrect format" in {
+    "produce undefined for result with incorrect format" in {
       MatchResult("2:0 (1:a,9:0)") must beEqualTo(Undefined)
     }
 
-    "produce score 3:0 a match without detailed scores like 3:0" in {
-      val score = Score(3, 0)
-      MatchResult("3:0") must beEqualTo(Overall(score))
+    "produce overall score 3:0 a match without detailed scores like 3:0" in {
+      MatchResult("3:0") must beEqualTo(Overall(Score(3, 0)))
     }
 
     "not produce score for '3:a' (wrong format)" in {
@@ -56,6 +55,49 @@ class MatchResultSpec extends Specification {
 
     "not produce forfeit for 'ff'" in {
       MatchResult("ff") must beEqualTo(Undefined)
+    }
+  }
+
+  "parse match result from games, score and forfeit" should {
+
+    "produce detailed with 1 game" in {
+      val games = List(Game(1, 0))
+      MatchResult(games) must beEqualTo(Detailed(games))
+    }
+
+    "produce detailed with 2 games" in {
+      val games =  List(Game(1, 0), Game(9, 0))
+      MatchResult(games) must beEqualTo(Detailed(games))
+    }
+
+    "produce overall with score" in {
+      val score = Score(3,0)
+      MatchResult(score = Some(score)) must beEqualTo(Overall(score))
+    }
+
+    "produce forfeit" in {
+      MatchResult(forfeit = Some(1)) must beEqualTo(Forfeit(1))
+    }
+  }
+
+  "deconstruct match result to string" should {
+
+    "produce 1:0 (1:0) from detailed" in {
+      val games = List(Game(1, 0))
+      MatchResult.unapply(Detailed(games)).get must equalTo("1:0 (1:0)")
+    }
+
+    "produce 2:0 (1:0,1:0) from detailed" in {
+      val games = List(Game(1, 0), Game(1, 0))
+      MatchResult.unapply(Detailed(games)).get must equalTo("2:0 (1:0,1:0)")
+    }
+
+    "produce 2:0 from overall" in {
+      MatchResult.unapply(Overall(Score(2, 0))).get must equalTo("2:0")
+    }
+
+    "produce f1 from forfeit" in {
+      MatchResult.unapply(Forfeit(1)).get must equalTo("f1")
     }
   }
 }
