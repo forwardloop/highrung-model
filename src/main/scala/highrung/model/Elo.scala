ef9a985cddf813166dead762fb71af8f8af5e8fa
kris <kris.lachor@gmail.com>
2018-03-12 15:44:44 +0000

    Custom EloResult - draft

--- src/main/scala/highrung/model/Elo.scala
+++ src/main/scala/highrung/model/Elo.scala
@@ -1,5 +1,6 @@
 package highrung.model
 
+import forwardloop.glicko2s.{ Draw, EloResult }
 import org.joda.time.DateTime
 
 case class Elo(
@@ -11,6 +12,7 @@ case class Elo(
   glickoVolatility: Double,
   createdTs: DateTime)
 
+/* Classification of ELO rating deviation in Glicko1 scale */
 object EloRatingDeviation {
   final val SmallDeviationThreshold = 80
   final val MediumDeviationThreshold = 150
@@ -28,3 +30,63 @@ sealed trait EloRatingDeviation
 case object SmallRatingDeviation extends EloRatingDeviation
 case object MediumRatingDeviation extends EloRatingDeviation
 case object LargeRatingDeviation extends EloRatingDeviation
+
+/*
+ * Extended definitions of `ELOResult`s to tailor to squash, to differentiate, say, 3:2 win from 3:0
+ *
+ * @see <a href="https://github.com/forwardloop/glicko2s/blob/master/src/main/scala/forwardloop/glicko2s/Glicko2.scala">Glicko2#EloResult</a>
+ */
+object EloResult {
+  final val StringWinVal = 0.9
+  final val WeakWinVal = 0.8
+
+  import EloResultType._
+
+  //  private def opponentResult(eloRes: EloResult) = eloRes match {
+  //    case Win =>
+  //
+  //  }
+
+  def apply(matchResult: MatchResult): EloResult = {
+    matchResult match {
+      case Forfeit(pId) => EloUndefined
+      case Overall(score) => EloUndefined
+      case Detailed(games) =>
+        MatchResult.gamesToScore(games) match {
+          case Score(p1, p2) =>
+            if (p1 == p2) Draw
+            else if (p1 > p2) {
+              if (p2 == 0) StraightSetWin
+              else if (p1 == p2 + 1) WeakWin
+              else StrongWin
+            } else {
+              if (p1 == 0) StraightSetLoss
+              else if (p2 == p1 + 1) WeakLoss
+              else StrongLoss
+            }
+          case _ => EloUndefined
+        }
+      case Undefined => EloUndefined
+    }
+  }
+}
+
+import EloResult._
+
+trait StraightSetResult extends EloResult
+trait StrongResult extends EloResult
+trait WeakResult extends EloResult
+trait AWin extends EloResult
+trait ALoss extends EloResult
+
+object EloResultType {
+  case object StraightSetWin extends StraightSetResult with AWin { val value = 1.0 } //e.g. 3:0
+  case object StrongWin extends StrongResult with AWin { val value = StringWinVal } //e.g. 3:1
+  case object WeakWin extends WeakResult with AWin { val value = WeakWinVal } //e.g. 3:2
+  case object StraightSetLoss extends StraightSetResult with ALoss { val value = 1.0 - StraightSetWin.value } //e.g. 0:3
+  case object StrongLoss extends StrongResult with ALoss { val value = 1.0 - StrongWin.value } //e.g. 1:3
+  case object WeakLoss extends WeakResult with ALoss { val value = 1.0 - WeakWin.value } //e.g. 2:3
+  case object EloUndefined extends EloResult { val value = -1.0 }
+
+  val eloResults = Seq(StraightSetWin, StrongWin, WeakWin, StraightSetLoss, WeakLoss, EloUndefined)
+}
--- src/test/scala/highrung/model/EloSpec.scala
+++ src/test/scala/highrung/model/EloSpec.scala
@@ -1,5 +1,6 @@
 package highrung.model
 
+import highrung.model.EloResultType._
 import org.specs2.mutable.Specification
 import EloRatingDeviation._
 
@@ -10,6 +11,35 @@ class EloSpec extends Specification {
       EloRatingDeviation(SmallDeviationThreshold - 1) === SmallRatingDeviation
       EloRatingDeviation(MediumDeviationThreshold - 1) === MediumRatingDeviation
       EloRatingDeviation(MediumDeviationThreshold + 1) === LargeRatingDeviation
+    }
+  }
+
+  "Elo result" should {
+
+    def r(s: String) = MatchResult(s)
+
+    "be correctly recognised for straight set win" in {
+      EloResult(r("3:0 (9:0,9:0,9:0)")) === StraightSetWin
+    }
+
+    "be correctly recognised for strong win" in {
+      EloResult(r("3:1 (9:0,9:0,0:9,9:0)")) === StrongWin
+    }
+
+    "be correctly recognised for weak win" in {
+      EloResult(r("3:2 (9:0,9:0,0:9,0:9,9:0)")) === WeakWin
+    }
+
+    "be correctly recognised for straight set loss" in {
+      EloResult(r("0:3 (0:9,0:9,0:9)")) === StraightSetLoss
+    }
+
+    "be correctly recognised for strong loss" in {
+      EloResult(r("1:3 (0:9,0:9,9:0,0:9)")) === StrongLoss
+    }
+
+    "be correctly recognised for weak loss" in {
+      EloResult(r("2:3 (0:9,0:9,9:0,9:0,0:9)")) === WeakLoss
     }
   }
 }
